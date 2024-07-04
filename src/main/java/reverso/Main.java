d4d8bfaab2e74a2f42ac532b9c4559786978a22e
Bondar Anton <bondar8524567913@gmail.com>
2024-07-04 22:51:10 +0200

    small changes

--- src/main/java/reverso/Main.java
+++ src/main/java/reverso/Main.java
@@ -1,18 +1,13 @@
 package reverso;
 
-import reverso.data.response.impl.*;
-import reverso.supportedLanguages.Language;
 
-import java.util.Arrays;
 
 
 public class Main {
     public static void main(String[] args) {
         Reverso reverso = new Reverso();
 
-        ContextResponse response = reverso.getContext(Language.POLISH,Language.PORTUGUESE,"какой");
+       reverso.getSpellCheck();
 
-        System.out.println(response.toJson());
-        System.out.println(Arrays.toString(response.getTranslations()));
     }
 }
